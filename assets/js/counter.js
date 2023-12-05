831da70d17858407787040ea43d96aa0d018d28b
gioarca <giorgio.arcamone93@gmail.com>
2023-12-05 10:45:14 +0100

    Modifiche eseguite correttamente

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,5 +1,41 @@
-let btnincrease = document.querySelector("#increase")
-let btndecrease = document.querySelector("#decrease")
+const h1 = document.createElement('h1');
+h1.textContent = "Counter";
+
+const p = document.createElement('p');
+p.textContent = "This is a small excerise to see how javascript works. Use the buttons below to increase (+) and decrease (-) the number inside the counter."
+p.setAttribute('id', 'element-insert');
+p.setAttribute('class', 'text-normal padding');
+
+const p1 = document.createElement('p');
+p1.textContent = "Counter: ";
+p1.setAttribute('id', 'Counter');
+p1.setAttribute('class', 'Counter');
+
+let result = document.createElement('label');
+result.textContent = "0";
+result.setAttribute('id', 'result');
+
+const buttonincrease = document.createElement('button-increase');
+buttonincrease.textContent = "+";
+buttonincrease.setAttribute('type', 'button');
+buttonincrease.setAttribute('class', 'increase');
+
+
+const buttondecrease = document.createElement('button-decrease');
+buttondecrease.textContent = "-";
+buttondecrease.setAttribute('type', 'button');
+buttondecrease.setAttribute('class', 'decrease');
+
+document.querySelector('#container').append(h1);
+document.querySelector('#container').append(p);
+document.querySelector('#container').append(p1);
+document.querySelector('#container').append(result);
+document.querySelector('#container').append(buttonincrease);
+document.querySelector('#container').append(buttondecrease);
+
+
+let btnincrease = document.querySelector(".increase")
+let btndecrease = document.querySelector(".decrease")
 
 let counter = 0
 
